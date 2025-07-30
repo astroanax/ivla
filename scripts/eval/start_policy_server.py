285142121ec194a5d1d8eac480963e233cad4a52
tianshihan <tianshihan@pjlab.org.cn>
2025-07-30 10:19:51 +0000

    !4 [fix] parse host and port for policy server from cli * [fix] parse host and port for policy server from cli

--- scripts/eval/start_policy_server.py
+++ scripts/eval/start_policy_server.py
@@ -1,10 +1,32 @@
-from run_configs.eval.dp_on_genmanip import eval_cfg
+"""
+This script is used to start the policy server for providing policy services to the evaluator.
+
+Usage:
+python scripts/eval/start_policy_server.py --host [host_address] --port [port_number]
+
+Arguments:
+--host: Server host address, defaults to localhost
+--port: Server port number, defaults to 5000
+
+Note:
+- This server needs to work in conjunction with the evaluator
+- Ensure the specified port is not already in use
+"""
+
+from internmanip.configs import ServerCfg
 from internmanip.agent.utils import PolicyServer
+import argparse
 
 
 def main():
-    server = PolicyServer(eval_cfg.agent.server_cfg)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--port", type=int, default=5000)
+    args = parser.parse_args()
+    server_cfg = ServerCfg(server_host=args.host, server_port=args.port)
+
+    server = PolicyServer(server_cfg)
     server.run()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
