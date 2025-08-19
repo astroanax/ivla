349cb04452e23cc2348c04d291697c122bdf5afa
zhouyunsong <zhouyunsong@pjlab.org.cn>
2025-08-19 10:28:31 +0000

    !55 update video encoder and pi0 on genmanip * update video encoder and pi0 on genmanip

--- run_configs/eval/dp_on_genmanip.py
+++ run_configs/eval/dp_on_genmanip.py
@@ -8,7 +8,7 @@ eval_cfg = EvalCfg(
         agent_type='dp_clip',
         base_model_path='/PATH/TO/YOUR/FINETUNED_CHECKPOINT',
         agent_settings={
-            'data_config': 'genmanip_v1',
+            'data_config': 'aloha_v3',
             'embodiment_tag': 'new_embodiment',
             'pred_action_horizon': 16,
             'adaptive_ensemble_alpha': 0.5,
--- run_configs/eval/pi0_on_genmanip.py
+++ run_configs/eval/pi0_on_genmanip.py
@@ -1,12 +1,12 @@
 from internmanip.configs import *
-from internmanip.benchmarks.genmanip.config.env_config import FrankaCameraEnable, AlohaSplitCameraEnable
+from pathlib import Path
 
 
 eval_cfg = EvalCfg(
     eval_type='genmanip',
     agent=AgentCfg(
-        agent_type='gr00t_n1_5_genmanip',
-        base_model_path='/PATH/TO/YOUR/FINETUNED_CHECKPOINT',
+        agent_type='pi0',
+        base_model_path='/PATH/TO/YOUR/PI0_FINETUNED_CHECKPOINT',
         agent_settings={
             'data_config': 'aloha_v3',
             'embodiment_tag': 'new_embodiment',
@@ -18,7 +18,7 @@ eval_cfg = EvalCfg(
         },
         server_cfg=ServerCfg(
             server_host='localhost',
-            server_port=5000
+            server_port=5000,
         ),
     ),
     env=EnvCfg(
@@ -26,7 +26,7 @@ eval_cfg = EvalCfg(
         env_settings=GenmanipEnvSettings(
             # Optional (if you have local path to the dataset): 
             # dataset_path='./data/dataset/IROS-2025-Challenge-Manip/validation',
-            res_save_path=f'{Path(__file__).absolute().parents[2]}/logs/eval/dp_on_genmanip',
+            res_save_path=f'{Path(__file__).absolute().parents[2]}/logs/eval/pi0_on_genmanip',
             is_save_img=False,
             aloha_split_camera_enable=AlohaSplitCameraEnable(
                 top_camera=True, left_camera=True, right_camera=True
