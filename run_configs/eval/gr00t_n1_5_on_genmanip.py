5f0ea4c50aa3d0154f7150c28abd06dbb59d1c57
zhouyunsong <zhouyunsong@pjlab.org.cn>
2025-08-19 10:34:22 +0000

    !57 update yaml files * Merge branch 'develop' into 'public' * 更新文件: gr00t_n1_5_on_genmanip.py * !56 merge public to devcelop * update video encoder and pi0 on genmanip * Merge branch 'develop' into 'public' * !53 [feat] add soft/hard task sr selection * !52 [feat] optimizing parallel eval  & update doc * !51 update the genmanip data downloading * !50 重新修改slice，支持default to None * 更新文件: agent_cfg.py * 更新文件: start_evaluator.py * 更新文件: base.py * 更新文件: dp_cfg.py * 更新文件: dp_clip.py

--- run_configs/eval/gr00t_n1_5_on_genmanip.py
+++ run_configs/eval/gr00t_n1_5_on_genmanip.py
@@ -5,7 +5,7 @@ from internmanip.benchmarks.genmanip.config.env_config import FrankaCameraEnable
 eval_cfg = EvalCfg(
     eval_type='genmanip',
     agent=AgentCfg(
-        agent_type='gr00t_n1_5_genmanip',
+        agent_type='gr00t_n1_5',
         base_model_path='/PATH/TO/YOUR/FINETUNED_CHECKPOINT',
         agent_settings={
             'data_config': 'aloha_v3',
@@ -26,7 +26,7 @@ eval_cfg = EvalCfg(
         env_settings=GenmanipEnvSettings(
             # Optional (if you have local path to the dataset): 
             # dataset_path='./data/dataset/IROS-2025-Challenge-Manip/validation',
-            res_save_path=f'{Path(__file__).absolute().parents[2]}/logs/eval/dp_on_genmanip',
+            res_save_path=f'{Path(__file__).absolute().parents[2]}/logs/eval/gr00t_n1_5_on_genmanip',
             is_save_img=False,
             aloha_split_camera_enable=AlohaSplitCameraEnable(
                 top_camera=True, left_camera=True, right_camera=True
